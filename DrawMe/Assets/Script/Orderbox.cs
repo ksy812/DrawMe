de915dd1a900057d5c09b2f38c64868e356e2628
Jisu Kim <indexkim33@gmail.com>
2022-06-07 18:25:38 +0900

    고객 대사 상자, 수락버튼 프리맵으로 수정 / dontdestroyonload객체로 만들기
    - 대사 상자, 수락 버튼 장면 넘어왔을 때 삭제 안되게 dontdestroyonload객체로 만듦.
    - dondestroyonload 는 자식 객체는 사용 못함. -> 삭제하지 않을 객체들만 모아놓은 캔버스 생성하여 처리
    - dondestroyonload 처리되면 캔버스 우선 순위가 밀림? 배경을 ui로 해놔서 그런지 새 장면의 ui 가려서 안보임
    
    해결방안
    1: dondestroyonload 객체의 우선순위 높이는 방법 찾아보기
    2 : 배경을 canvas로 만들지 않기?

---
+++ DrawMe/Assets/Script/Orderbox.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Orderbox : MonoBehaviour
+{
+    public Text text;
+    
+    public void setText(string o)
+    {
+        text.text = o;
+    }
+}
