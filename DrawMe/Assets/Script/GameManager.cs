59db464d9edab594942d9671b144202b2c31227d
Kim So Yeon <s2023@e-mirim.hs.kr>
2022-06-10 05:09:36 +0900

    화면 캡쳐 크기 설정(#10)
    캔버스 사이즈만큼만 캡쳐하도록 수정

--- DrawMe/Assets/Script/GameManager.cs
+++ DrawMe/Assets/Script/GameManager.cs
@@ -33,21 +33,22 @@ public class GameManager : MonoBehaviour
 
     public void OnClickComplete()
     {
-        //Capture();
+        Capture();
         Customer.satisfaction = 10; //나중에 모델 따라 만족도 설정
         drawingManager.SetClear();
     }
 
     private void Capture()
     {
-        RenderTexture renderTexture = new RenderTexture(Screen.width, Screen.height, 24); //1920 1200
+        RenderTexture renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
         cam.targetTexture = renderTexture;
 
-        Texture2D screenShot = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        Texture2D screenShot = new Texture2D(750, 800, TextureFormat.RGB24, false);
         //cam.Render();
         RenderTexture.active = renderTexture;
 
-        Rect area = new Rect(0f, 0f, Screen.width, Screen.height);
+        Rect area = new Rect(950, 540, 750, 800); //캡쳐할 영역 지정
+        //Rect area = new Rect(0f, 0f, Screen.width, Screen.height);
         screenShot.ReadPixels(area, 0, 0);
         screenShot.Apply();
 
