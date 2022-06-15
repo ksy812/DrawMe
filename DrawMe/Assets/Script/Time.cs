9216172cf55c48ca86a17f3238b28e062114029c
Jisu Kim <81640695+kimjisu33@users.noreply.github.com>
2022-06-15 15:24:49 +0900

    시간 1분 남았을 때 깜박거리기

--- DrawMe/Assets/Script/Time.cs
+++ DrawMe/Assets/Script/Time.cs
@@ -15,6 +15,7 @@ public class Time : MonoBehaviour
 
     long minutes;
 
+
     private void Awake()
     {
 
@@ -24,11 +25,16 @@ public class Time : MonoBehaviour
     
     void Update()
     {
-        //minutes = watch.ElapsedMilliseconds / 60000;
-        minutes = watch.ElapsedMilliseconds / 1000; //초
+        minutes = watch.ElapsedMilliseconds / 60000;
+        //minutes = watch.ElapsedMilliseconds / 1000; //초
         time.text = minutes + "";
-        if (minutes % 2 == 0 && minutes != 0) ;  //2분 지날때 마다 바뀜
+        //if (minutes % 2 == 0 && minutes != 0) ;  //2분 지날때 마다 바뀜
         if (minutes == 3) icon_time.sprite = moon; //3분 되면 저녁 그림
+        if (minutes >= 5)
+        {
+            if(watch.ElapsedMilliseconds / 1000 % 2 == 0) time.color = Color.red;
+            else time.color = Color.white;
+        }
         if (minutes >= 6 && Customer.object_is_destory) //6분 되면 게임 종료
         {
             UnityEngine.Debug.Log("엔딩씬 호출");
