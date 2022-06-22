871ac5daff976de72b618e8da879085775e2b61c
Jisu Kim <indexkim33@gmail.com>
2022-06-22 09:57:13 +0900

    엔딩 씬 수정

--- DrawMe/Assets/Script/EndingManager.cs
+++ DrawMe/Assets/Script/EndingManager.cs
@@ -6,20 +6,28 @@ using UnityEngine.UI;
 public class EndingManager : MonoBehaviour
 {
     public Image ending;
+    public Text good;
+    public Text bad;
     public Sprite[] ending_back;
 
     bool is_success;
     private void Start()
     {
+
         if (Money.amount > 20) is_success = true;
         else is_success = false;
 
         if (is_success)
         {
             ending.sprite = ending_back[0];
-        }else
+            good.text = Money.amount + "";
+            bad.text = "";
+        }
+        else
         {
             ending.sprite = ending_back[1];
+            bad.text = Money.amount + "";
+            good.text = "";
         }
     }
 }
